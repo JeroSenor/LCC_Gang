708b8443047fb79f54aa4a8249b5ae409d627b0a
JeroSenor <57153032+JeroSenor@users.noreply.github.com>
2021-03-29 18:21:14 -0300

    Create terminal.c

---
+++ weekly_tp_29-03-2021/terminal.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int convert_buffer_a_argv (char buffer[2550], char argv[10][255]) {
+
+    char* token;
+    token = strtok(buffer, " ");
+    int  counter = 0;
+
+    while( token != NULL) {
+        strcpy(argv[counter],token);
+
+        token = strtok(NULL, " ");
+        counter++;
+    }
+
+return counter;
+
+}
+
+
+void main () {
+    // Toma como máximo 9 argumentos de 254 char + el nulo
+    // (despues del ultimo argumento el puntero debe ser NULL
+    // para indicar el fin de argv)
+    char argv[10][255];
+    char buffer[2550];
+    while (1) {
+        printf("RECURSION@MACHINE:~$ ");
+        gets(buffer);
+        int cant_arg = convert_buffer_a_argv(buffer, argv);
+        pid_t pid = fork();
+
+        // hijo
+        if (pid == 0){
+            execv(argv[0], argv);
+            printf("hijo: %d\n", getpid());
+        }
+        else {
+            if ( *argv[cant_arg - 1] != '&')
+                waitpid(pid, NULL, 0);
+        }
+    }
+    free(argv);
+}
